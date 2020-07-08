400425d48937ce27e17264a4733bafd3944c4806
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-07-08 09:42:42 +0530

    add lists

---
+++ vue-noob-COND-1/scripts.js
@@ -0,0 +1,64 @@
+new Vue({
+    el: '#dom-1',
+    data: {
+        show1: true,
+        show2: true,
+        show3: 'vue'
+    }
+});
+
+
+new Vue({
+    el: '#dom-2',
+    data: {
+        show: true,
+    }
+});
+
+new Vue({
+    el: '#dom-3',
+    data: {
+        show: true,
+    }
+});
+
+new Vue({
+    el: '#dom-4',
+    data: {
+        frameworks: ['React JS', 'Vue JS', 'Angular JS']
+    }
+});
+
+new Vue({
+    el: '#dom-5',
+    data: {
+        frameworks: ['React JS', 'Vue JS', 'Angular JS']
+    }
+});
+
+
+new Vue({
+    el: '#dom-6',
+    data: {
+        frameworks: [{
+                name: 'React JS',
+                founder: 'Jordan Walke'
+            },
+            {
+                name: 'Vue JS',
+                founder: 'Evan You',
+            },
+            {
+                name: 'Angular JS',
+                founder: 'Misko Hevery'
+            }
+        ]
+    }
+});
+
+new Vue({
+    el: '#dom-7',
+    data: {
+        frameworks: ['React JS', 'Vue JS', 'Angular JS']
+    }
+});
