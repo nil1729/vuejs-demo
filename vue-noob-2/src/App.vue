86b0313aa4b17050de2df8547dbe2dc15beb78bd
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-07-11 22:26:52 +0530

    api change

--- vue-noob-2/src/App.vue
+++ vue-noob-2/src/App.vue
@@ -35,7 +35,7 @@ export default {
     const vm = this;
     axios
       .get(
-        "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?format=json&f_has_lyrics=2&quorum_factor=1&apikey=83b9c178f2228a8c4ea844a1e662c119"
+        "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=10&f_has_lyrics=1&apikey=83b9c178f2228a8c4ea844a1e662c119"
       )
       .then(res => (vm.tracks = res.data.message.body.track_list))
       .catch(err => console.log(err));
